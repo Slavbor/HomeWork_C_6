a19292a31c8fb72ab5b1dc600a1624c3f784b4e2
Slavbor <shipmate@mail.ru>
2023-01-22 03:44:01 +0300

    Zadacha 41 completed

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+using static System.Console;
+Clear();
+Write("Enter any numbers separated by space: ");
+
+string[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)!;
+
+WriteLine(AboveZeroCalculation(array));
+
+
+
+
+
+
+
+int AboveZeroCalculation(string[] arrayCalc)
+{
+
+    int count = 0;
+    foreach (var element in arrayCalc)
+    {
+        if(int.Parse(element) > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+} 
